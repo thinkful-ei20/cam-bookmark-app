f51db1540e67fd52ff4b4ff43c5df88b17ca1016
Cam Prier <CSPrier@gmail.com>
2018-04-12 09:09:48 -0700

    Added filter rating option to html, added ajax calls to api.js

--- scripts/api.js
+++ scripts/api.js
@@ -4,7 +4,6 @@
 // API module methods are async functions for returning responses
 const api = (function() {
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/cam';
-
   // GET - Request data from a specified resource
   const getBookmarks = (callback) => {
     // Load JSON-encoded data from the server using the GET HTTP request
@@ -12,18 +11,44 @@ const api = (function() {
   };
 
   // POST - submit data to be processed to a specified resource
-  const createBookmark = (name, callback) => {
-   
+  const createBookmark = (data, callback, errorCallback) => {
+    $.ajax({
+      url:`${BASE_URL}/bookmarks`,
+      method: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify(data),
+      success: callback,
+      error: errorCallback,
+    });
   };
 
   // PATCH 
-  const updateBookmark = (id, updateData, callback) => {
-
+  const updateBookmark = (id, updateData, callback, errorCallback) => {
+    $.ajax({
+      url:`${BASE_URL}/bookmarks/${id}`,
+      method: 'PATCH',
+      contentType: 'application/json',
+      data: JSON.stringify(updateData),
+      success: callback,
+      error: errorCallback,
+    });
   };
 
-
+  // DELETE
+  const deletedBookmark = (id, callback, errorCallback) => {
+    $.ajax({
+      url:`${BASE_URL}/bookmarks/${id}`,
+      method: 'DELETE',
+      success: callback,
+      error: errorCallback,
+    });
+  };
 
   return {
+    getBookmarks,
+    createBookmark,
+    updateBookmark,
+    deletedBookmark,
 
   };
 }());
